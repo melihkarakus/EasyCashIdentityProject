d2689f8cbb0b35922ce36a7cb505f68b6f614428
Melih karakuş <melih.karakus1818@gmail.com>
2023-10-22 15:40:05 +0300

    6.  ders sonu

---
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-VLOIQ6M3; initial catalog=EasyCashDb; integrated security=true");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; } //Database Tabloyu yansıtmak için tanımlanan kod
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccount
+    {
+        public int CustomerAccountID { get; set; } //Database yansıtılcak veriler
+        public string CustomerAccounNumber { get; set; }
+        public string  CustomerAccountCurrency { get; set; }
+        public decimal CustomerAccountBalans { get; set; }
+        public string BankBranch { get; set; }
+        public int AppUserID { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
