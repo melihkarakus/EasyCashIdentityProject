0506ef304d39b953124fb8d10a5e8e0a50cbc14f
Melih karakuş <melih.karakus1818@gmail.com>
2023-10-23 22:23:15 +0300

    12. Video Sonu

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,67 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;//usermanager sınıfından bir appuser bağlı türetim yaptık.
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+        {
+			if (ModelState.IsValid)
+			{
+				// ModelState, sayfanın sunum katmanında doğrulama kurallarının geçip geçmediğini kontrol eder.
+
+				AppUser appUser = new AppUser()
+				{
+					UserName = appUserRegisterDto.Username,
+					Name = appUserRegisterDto.Name,
+					Surname = appUserRegisterDto.Surname,
+					Email = appUserRegisterDto.Email,
+					City = "Veri Gelecek",
+					District = "Veri Gelecek",
+					ImageUrl = "Veri Gelecek"
+				};
+
+				// Yeni bir AppUser nesnesi oluşturulur ve kullanıcı tarafından sağlanan verilerle doldurulur.
+
+				var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+
+				// _userManager, kullanıcı hesaplarını yönetmek için bir kimlik doğrulama servisini temsil eder ve 
+				// CreateAsync yöntemi ile yeni bir kullanıcı hesabı oluşturulur.
+
+				if (result.Succeeded)
+				{
+					// Kullanıcı hesabının başarıyla oluşturulup oluşturulmadığı kontrol edilir ve başarılıysa aşağıdaki işlem yapılır:
+					// Yönlendirme yapılır ve kullanıcıyı "ConfirmMail" adlı controller'ın "Index" adlı action'ına yönlendirir.
+
+					return RedirectToAction("Index", "ConfirmMail");
+				}
+				else
+				{
+					foreach (var item in result.Errors) //ekrana password ile alakalı hata çıkartmak istiyorsak bu kodu çalıştırabiliriz
+					{
+						ModelState.AddModelError("", item.Description); //key değeri boşluk bırakıp item.description yani açıklaması
+					}
+				}
+			}
+
+			// Eğer ModelState doğrulamayı geçemezse veya kullanıcı hesabı oluşturulamazsa, kullanıcıyı aynı sayfada tutar ve View() yöntemiyle
+			// aynı sayfanın görünümünü geri döndürür.
+			return View();
+		}
+    }
+}
---
+++ EasyCashIdentityProject.PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+	//kimlik doğrulama hatasının açıkça gösterilmesi veya özelleştirilmesi
+	public class CustomIdentityValidator : IdentityErrorDescriber
+	{
+		//override methodun işleyişi bozulmadan bizim istediğimiz tarzda yazılmasını sağlıyoruz
+		public override IdentityError PasswordTooShort(int length)//Errors isimleri overrider içerikli tanımlarız
+		{
+			return new IdentityError()//yeni bir identity Error mesajı oluşturduk
+			{
+				Code = "PasswordTooShort",//Buraya verilen başlık 
+				Description = $"Parolan en az {length} karakter olmalıdır.",//Burası kendi açıklamamız 
+			};
+		}
+		public override IdentityError PasswordRequiresUpper()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresUpper",
+				Description = "En az 1 büyük harf giriniz.",
+			};
+		}
+		public override IdentityError PasswordRequiresLower()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresLower",
+				Description = "En az 1 küçük harf giriniz."
+			};
+		}
+		public override IdentityError PasswordRequiresDigit()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresDigit",
+				Description = "En az 1 rakam giriniz.",
+			};
+		}
+		public override IdentityError PasswordRequiresNonAlphanumeric()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresNonAlphanumeric",
+				Description = "En az 1 sembol giriniz {!,#,/,+}."
+			};
+		}
+	}
+}
